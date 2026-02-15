3d41f65f88057e96a1a418184daa5b174adbb66f
GuSalesDev <gustavosales6624@gmail.com>
2026-02-15 12:18:05 -0300

    taskday1
    Task diaria 1.

---
+++ CadstroUser/src/Usuario.java
@@ -0,0 +1,53 @@
+public class Usuario {
+    
+    // Atributos da classe Usuario
+
+    private String nome;
+    private int idade;
+    private String email;
+
+    //Contrutor da classe Usuario
+
+    public Usuario(String nome, int idade, String email) {
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+    }
+
+    //Getters e Setters para os atributos da classe Usuario
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Metodo tostring sobrescrito para exibir as informações do usuario de forma legivel
+
+    @Override
+public String toString() {
+    return "Usuario:\n" +
+           "Nome: " + nome + "\n" +
+           "Idade: " + idade + "\n" +
+           "Email: " + email + "\n";
+    }
+
+}
